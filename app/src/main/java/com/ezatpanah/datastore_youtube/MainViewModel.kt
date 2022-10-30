03f9bc734c0e97d3f579e2b004d407c2abc28de9
Reyhaneh Ezatpanah <ezatpanah@gmail.com>
2022-10-30 16:18:03 +0200

    Update MainViewModel.kt

--- app/src/main/java/com/ezatpanah/datastore_youtube/MainViewModel.kt
+++ app/src/main/java/com/ezatpanah/datastore_youtube/MainViewModel.kt
@@ -10,7 +10,12 @@ import kotlinx.coroutines.launch
 
 class MainViewModel (application: Application) : AndroidViewModel(application) {
 
-
+/**
+when we need to use context inside our Viewmodel we should use AndroidViewModel (AVM),
+because it contains the application context. To retrieve the context call getApplication(),
+otherwise use the regular ViewModel (VM).
+**/
+    
     private val dataStore = DataStoreManager(application)
 
     val getTheme = dataStore.getTheme().asLiveData(Dispatchers.IO)
